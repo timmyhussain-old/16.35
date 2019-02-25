501e40e21ae9b08b595fecd08c2b23349ae01acf
timmyhussain <timmyhussain@yahoo.com>
2019-02-25 02:09:27 -0500

    actual implementation 1

--- q2.c
+++ q2.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-  printf("int: %ld\n", sizeof(int));
-  printf("float: %ld\n", sizeof(float));
-  printf("double: %ld\n", sizeof(double));
-  printf("char: %ld\n", sizeof(char));
+int main(int argc, char **argv) {
+
+  double a, b;
+  char *s;
+  a = atof(argv[1]);
+  b = atof(argv[3]);
+  s = argv[2];
+
+  // printf("%d\n", atoi(s));
+  if (strcmp(s, "+") == 0)
+    printf("%.3f", a + b);
+  else if (strcmp(s, "-") == 0)
+    printf("%.3f", a - b);
+  else if (strcmp(s, "/") == 0)
+    printf("%.3f", a / b);
+  else if (strcmp(s, "x") == 0)
+    printf("%.3f", a * b);
+  else if (strcmp(s, "X") == 0)
+    printf("%.3f", a * b);
+  else
+    printf("%.3f", a * b);
 
   return 0;
 }
